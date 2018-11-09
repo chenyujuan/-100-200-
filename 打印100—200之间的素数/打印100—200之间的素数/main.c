274b62575e2388af58a3a99006310f61df0735cc
CHEN <2769046494@qq.com>
2018-11-09 18:40:43 +0800

    提交代码

---
+++ 打印100—200之间的素数/打印100—200之间的素数/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+  int i=0;
+  int j=0;
+
+  for(i=100; i<=200; i++)
+  {
+	  for (j=2; j<=i-1; j++)
+	  {
+	    if(i%j == 0)
+		break;
+	  }
+    if(j == i)
+	printf(" %d ",i);
+  }
+  printf("\n");
+  return 0;
+}
